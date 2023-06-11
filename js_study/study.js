a5a9eee20c60a990fc5ac30e172c1d4292d3c99a
rkdalswl718 <rkdalswl070718@gmail.com>
2023-06-11 12:28:17 +0900

    prompt

--- js_study/study.js
+++ js_study/study.js
@@ -1,23 +1,2 @@
-const calculator={
-    add : function(a,b){
-        alert(a+b);
-    },
-    minus: function(a,b){
-        alert(a-b);
-    },
-    multi: function(a,b){
-        alert(a*b)
-    },
-    div: function(a,b){
-        alert(a/b);
-    },
-    poweroff: function(a,b){
-        alert(a**b);
-    }
-};
-
-calculator.add(4,5);
-calculator.minus(5,6);
-calculator.multi(9,9);
-calculator.div(6,7);
-calculator.poweroff(3,3);
+const age = (parseInt(prompt("나이?")));
+console.log(age);

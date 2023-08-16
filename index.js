5d97dbbf93556eb9be5958b23da25729d430b5e6
rkdalswl718 <rkdalswl070718@gmail.com>
2023-08-16 20:29:42 +0900

    study

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-let i = 0;
+document.getElementById('heading');
 
-const down = document.querySelector("#down");
-const up = document.querySelector("#up");
-const h1 = document.querySelector("#h1");
-
-function upClicked () { //
-    h1.innerText = ++i; //+1씩 h1에서 더함
+const click = ('click') = > {
+    heading.style.color='red';
 }
-
-function downClicked () { // 
-    h1.innerText = --i; // -1씩 h1에세 뻄
-}
-
-up.addEventListener("click",upClicked); //up에 클릭 포인트 추가
-down.addEventListener("click",downClicked); //down에 클릭 포인트 추가
